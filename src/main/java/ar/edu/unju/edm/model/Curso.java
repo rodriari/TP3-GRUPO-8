c994a75c2d10022b98d3ea961932ba4b81248d0b
Franco Alejandro Alancay <francoalancayomg369@gmail.com>
2022-06-08 16:33:34 -0300

    Base de Datos-AlancayFranco

--- src/main/java/ar/edu/unju/edm/model/Curso.java
+++ src/main/java/ar/edu/unju/edm/model/Curso.java
@@ -13,34 +13,60 @@ import javax.validation.constraints.Size;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Component;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+@Entity
 @Component 
+@Table (name = "LISTACURSO")
 public class Curso {
+	@Id 
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column (name= "ID", nullable = true)
 	private int id;
 	@Size(min=3, max=100, message="EL nombre debe tener 3 caracteres minimo, maximo 100")
 	@NotBlank(message="El nombre no puede ser espacios en blanco")
 	@NotEmpty(message="El nombre no puede estar vacio")
+	
+	@Column (name = "NOMBRE", nullable = true)
 	private String nombre;
 	@Size(min=0, max=200, message="La descripcion debe tener maximo 100 caracteres")
 	@NotBlank(message="La descripcion no puede ser espacios en blanco")
+	
+	@Column (name = "DESCRIPCION")
 	private String descripcion;
 	@DateTimeFormat(pattern="yyyy-MM-dd")
+	
+	@Column (name = "FEACHADENACIMIENTO")
 	private LocalDate fechainicio;
 	@PositiveOrZero(message="El costo debe ser mayor o igual a 0")
+	
+	@Column (name = "COSTO")
 	private int costo;
 	@Min(value=0, message="La valoracion no puede ser menor que 0")
 	@Max(value=10, message="La valoracion no puede ser mayor que 10")
+	
+	@Column (name = "VALORACION")
 	private int valoracion;
 	@Positive(message="La duracion debe ser mayor a 0 semanas")
+	
+	@Column (name = "DURACELL")
 	private int duracion;
 	@Size(min=3, max=30, message="El docente debe tener 3 caracteres minimo, maximo 30")
 	@NotBlank(message="El docente no puede ser espacios en blanco")
 	@NotEmpty(message="El docente no puede estar vacio")
+	
+	@Column (name = "DOCENTE")
 	private String docente;
 	@Positive(message="El cupo debe ser positivo")
+	
 	private int cupo;
 
 	private Boolean estado;
-	
+
 	public Boolean getEstado() {
 		return estado;
 	}
--- src/main/java/ar/edu/unju/edm/model/Usuario.java
+++ src/main/java/ar/edu/unju/edm/model/Usuario.java
@@ -2,8 +2,12 @@ package ar.edu.unju.edm.model;
 
 import java.time.LocalDate;
 
-
-
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
@@ -13,33 +17,45 @@ import javax.validation.constraints.Size;
 
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Component;
-
+@Entity
 @Component
+@Table (name = "LISTAUSUARIO")
 public class Usuario {
 
+	@Id 
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column (name= "DNI", nullable = true)
+	@Min(value=1000000, message="El dni debe ser mayor a 1.000.000")
+	@Max(value=99999999, message="El dni debe ser menor a 99.999.999")
+	private int dni;
+	
 	@NotEmpty //@Email
+	@Column (name= "MAIL", nullable = true)
 	private String email;
 	
 	@Size(min=4, max=30, message="La contraseña debe tener 4 caracteres minimo, maximo 30")
 	@NotBlank(message="La contraseña no puede ser espacios en blanco")
+	
+	@Column (name= "CONTRASEÑA", nullable = true)
 	private String contraseña;
 	
 	@Size(min=2, max=50, message="El apellido debe tener 2 caracteres minimo, maximo 50")
 	@NotBlank(message="El apellido no puede ser espacios en blanco")
 	@NotEmpty(message="El apellido no puede estar vacio")
+	
+	@Column (name= "APELLIDO")
 	private String apellido;
 	
 	@Size(min=2, max=50, message="EL nombre debe tener 2 caracteres minimo, maximo 50")
 	@NotBlank(message="El nombre no puede ser espacios en blanco")
 	@NotEmpty(message="El nombre no puede estar vacio")
-	private String nombre;
 	
-	@Min(value=1000000, message="El dni debe ser mayor a 1.000.000")
-	@Max(value=99999999, message="El dni debe ser menor a 99.999.999")
-	private int dni;
+	@Column (name= "NOMBRE", nullable = true)
+	private String nombre;
 	
 	@Past(message="La fecha de nacimiento debe ser del pasado")
 	@DateTimeFormat(pattern="yyyy-MM-dd")
+	
 	private LocalDate fechanacimiento;
 	
 	private Boolean estado;
