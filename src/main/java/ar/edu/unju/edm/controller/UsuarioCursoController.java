0553753495f3095c3b23fa323f3cdf31c7106ceb
franc <franc@DESKTOP-N04TJU5>
2022-06-15 23:32:22 -0300

    cambios

--- src/main/java/ar/edu/unju/edm/controller/UsuarioCursoController.java
+++ src/main/java/ar/edu/unju/edm/controller/UsuarioCursoController.java
@@ -6,14 +6,12 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import ar.edu.unju.edm.model.Usuario;
 import ar.edu.unju.edm.model.UsuarioCurso;
 import ar.edu.unju.edm.service.ICursoService;
 import ar.edu.unju.edm.service.IUsuarioCursoService;
